69adf92085c13fb697acb4d10db246b8edd7b4be
shahabbukhari <shahabbukhari098@gmail.com>
2022-02-16 23:22:39 +0500

    :arrow_up: HackerRank
    LeaderBoard Problem

---
+++ 04 - Python/hackerrank/leaderBoard.py
@@ -0,0 +1,15 @@
+def climbingLeaderboard(ranked, player):
+    ranked = sorted(list(set(ranked)), reverse=True)
+    player = sorted(player, reverse=True)
+
+    l = len(ranked)
+    j = 0
+
+    ans = []
+    for i in range(len(player)):
+        while j < l and player[i] < ranked[j]:
+            j += 1
+
+        ans.append(j+1)
+
+    return ans[::-1]
